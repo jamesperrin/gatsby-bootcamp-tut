9624677c42c7bc56357fbe90ffe87d0b6335bf92
James Perrin <countrydawgg@gmail.com>
2019-07-25 10:38:19 -0700

    Finished section 11.
    11. Working with Markdown Posts
    - Timestamp: 2:03:37
    https://www.youtube.com/watch?v=8t0vNu2fCCM&t=7417s

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,22 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV}`,
+    path: `.env.${process.env.NODE_ENV}`
 });
 
 module.exports = {
-  siteMetadata: {
-    title: "Full-Stack Bootcamp",
-    author: "James Perrin"
-  },
+    siteMetadata: {
+        title: "Full-Stack Bootcamp",
+        author: "James Perrin"
+    },
     plugins: [
         "gatsby-plugin-sass",
         {
             resolve: "gatsby-source-filesystem",
             options: {
-                name: 'src',
+                name: "src",
                 path: `${__dirname}/src/`
             }
-        }
+        },
+        'gatsby-transformer-remark'
     ]
 };
