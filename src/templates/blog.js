b081e3e4779ae22e143fcbcc79c975e9c9383ef8
James Perrin <countrydawgg@gmail.com>
2019-07-29 12:46:30 -0700

    Finished Section 14. Rendering Post Data in Blog Template - Timestamp: 2:52:08 https://www.youtube.com/watch?v=8t0vNu2fCCM&t=10328s
    DIFF:
    
    Used ES6 Spread Operator in /templates/blog.js
    
    REF:
    https://www.gatsbyjs.org/docs/node-apis/#createPages
    https://www.gatsbyjs.org/docs/debugging-async-lifecycles/

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import Layout from '../components/layout';
+import React from "react";
+import Layout from "../components/layout";
+import { graphql } from "gatsby";
 
-const Blog = () => {
+export const query = graphql`
+    query($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            frontmatter {
+                title
+                date
+            }
+            html
+        }
+    }
+`;
+
+const Blog = props => {
+    const postData = { ...props.data.markdownRemark };
+
     return (
         <Layout>
-            This is the blog template
+            <h1>{postData.frontmatter.title}</h1>
+            <p>{postData.frontmatter.date}</p>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: postData.html
+                }}
+            ></div>
         </Layout>
     );
-}
+};
 
-export default Blog
+export default Blog;
