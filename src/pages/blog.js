6823e6d9dad45a034cdc6e746c40f15d0f2f929c
James Perrin <countrydawgg@gmail.com>
2019-07-30 17:49:09 -0700

    Finished 17. Rendering Contentful Posts - Timestamp: 3:38:29 https://www.youtube.com/watch?v=8t0vNu2fCCM&t=13109s

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,21 +1,19 @@
-import React from "react";
-import Layout from "../components/layout";
-import { Link, graphql, useStaticQuery } from "gatsby";
+import React from 'react';
+import Layout from '../components/layout';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import blogStyles from './blog.module.scss';
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
         query {
-            allMarkdownRemark {
+            allContentfulBlogPost(
+                sort: { fields: publishedDate, order: DESC }
+            ) {
                 edges {
                     node {
-                        frontmatter {
-                            title
-                            date
-                        },
-                        fields{
-                            slug
-                        }
+                        title
+                        slug
+                        publishedDate(formatString: "MMMM DD, YYYY")
                     }
                 }
             }
@@ -26,12 +24,15 @@ const BlogPage = () => {
         <Layout>
             <h1>Blog</h1>
             <ol className={blogStyles.posts}>
-                {data.allMarkdownRemark.edges.map(edge => {
+                {data.allContentfulBlogPost.edges.map(edge => {
                     return (
-                        <li  className={blogStyles.post}>
-                            <Link to={`/blog/${edge.node.fields.slug}`} title={`Post ${edge.node.fields.slug}`}>
-                                <h2>{edge.node.frontmatter.title}</h2>
-                                <p>{edge.node.frontmatter.date}</p>
+                        <li className={blogStyles.post}>
+                            <Link
+                                to={`/blog/${edge.node.slug}`}
+                                title={`Post ${edge.node.slug}`}
+                            >
+                                <h2>{edge.node.title}</h2>
+                                <p>{edge.node.publishedDate}</p>
                             </Link>
                         </li>
                     );
@@ -39,6 +40,6 @@ const BlogPage = () => {
             </ol>
         </Layout>
     );
-}
+};
 
 export default BlogPage;
