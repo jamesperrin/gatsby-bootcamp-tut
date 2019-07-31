5e9f6de1b108a6305a5f4b09722af64d2b9615c2
James Perrin <countrydawgg@gmail.com>
2019-07-30 17:11:23 -0700

    Finished Section 16. Getting Started with Contentful - Timestamp: 3:21:19 https://www.youtube.com/watch?v=8t0vNu2fCCM&t=12079s
    BUG: GraphQL Error Unknown type "ContentfulFixed"
    The following error message will be displayed.
    
    GraphQL Error Unknown type "ContentfulFixed". Did you mean "ContentfulBlogPost", "ContentfulContentType", or "ContentfulBlogPostEdge"?
    
    SOLUTION:
    (1) Within Contentful, click on Media.
    (2) Click on "Add Assest"
    (3) Upload at least one image file into your Contentful asset repository

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         author: 'James Perrin'
     },
     plugins: [
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                 spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+            }
+        },
         'gatsby-plugin-sass',
         {
             resolve: 'gatsby-source-filesystem',
