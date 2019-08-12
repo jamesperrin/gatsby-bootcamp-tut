8d33ee8898529944c11bd5dfee3a0749eef21796
James Perrin <countrydawgg@gmail.com>
2019-08-12 08:31:41 -0700

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => (
         <h2>
             I'm James, a full-stack developer living in beautiful Washington.
         </h2>
-        <p>
-            Need a developer? <Link to="/contact">Contact me</Link>.
-        </p>
     </Layout>
 );
 
